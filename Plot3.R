98aacea21d6162ddd83be09f0b3f1a1320cf332f
JefersonMonteiro <jefersonsantosmonteiro@gmail.com>
2019-04-08 11:53:24 -0300

    Add files via upload

---
+++ Plot3.R
@@ -0,0 +1,34 @@
+#install.packages("data.table")
+
+library("data.table")
+
+getwd()
+
+setwd("H:/DS/Exploratory-Data-Analysis/project1/data")
+
+#Reads data
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
+
+
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Filter Date
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("Plot3.png", width=480, height=480)
+
+# Plot 3
+plot(powerDT[, dateTime], powerDT[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_2],col="red")
+lines(powerDT[, dateTime], powerDT[, Sub_metering_3],col="blue")
+legend("topright"
+       , col=c("black","red","blue")
+       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       ,lty=c(1,1), lwd=c(1,1))
+
+dev.off()
